d0ba17fc2c4bc200cb79bbd2646f7f8eff24c3b4
hrecker <henryjrecker@gmail.com>
2020-06-10 21:46:25 -0500

    Updated tilemap to render from array, and added highlight to hovered tile

--- webroot/scripts/scenes/MapScene.js
+++ webroot/scripts/scenes/MapScene.js
@@ -20,6 +20,23 @@ export class MapScene extends Phaser.Scene {
         this.load.image('block1', 'assets/sprites/Block_1.png');
         this.load.image('block2', 'assets/sprites/Block_2.png');
         this.load.image('block3', 'assets/sprites/Block_3.png');
+
+        this.tileMap = []
+        this.tileMap.push([1, 2, 2]);
+        this.tileMap.push([1, 3, 1]);
+        this.tileMap.push([3, 2, 3]);
+        this.tileMapImages = [];
+        this.tileMapImages.push([1, 2, 2]);
+        this.tileMapImages.push([1, 3, 1]);
+        this.tileMapImages.push([3, 2, 3]);
+        this.mapWidth = 3;
+        this.mapHeight = 3;
+
+        this.mapOriginX = this.game.renderer.width / 2;
+        this.mapOriginY = this.game.renderer.height / 2 + 100;
+        
+        this.tileHighlightActiveX = -1;
+        this.tileHighlightActiveY = -1;
     }
 
     create() {
@@ -27,10 +44,7 @@ export class MapScene extends Phaser.Scene {
         this.add.image(0, 0, 'background').setOrigin(0, 0);
 
         // Blocks
-        this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, 'block1').setScale(0.5);
-        this.add.image(this.game.renderer.width / 2 - blockImageWidth / 2, this.game.renderer.height / 2 + blockImageWidth / 4, 'block2').setScale(0.5);
-        this.add.image(this.game.renderer.width / 2 + blockImageWidth / 2, this.game.renderer.height / 2 + blockImageWidth / 4, 'block3').setScale(0.5);
-        this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2 + blockImageWidth / 2, 'block1').setScale(0.5);
+        this.createTileMap();
 
         // UI
         let titleTextStyle = { font: "bold 32px Arial", fill: "#15b800", boundsAlignH: "center", boundsAlignV: "middle" };
@@ -55,6 +69,31 @@ export class MapScene extends Phaser.Scene {
         this.input.on("pointerup", this.addClickCash, this);
         
         console.log("created MapScene");
+    }
+
+    // Origin of tile map coordinates is the tile closest to the bottom of the screen.
+    // X increases diagonally up and to the right. Y increases diagonally up and to the left.
+    createTileMap() {
+        for (let x = this.mapWidth - 1; x >= 0; x--) {
+            for (let y = this.mapHeight - 1; y >= 0; y--) {
+                let xDiff = (x - y) * blockImageWidth;
+                let yDiff = (x + y) * -blockImageWidth / 2;
+                let tileImage = this.add.image(this.mapOriginX + xDiff, 
+                    this.mapOriginY + yDiff, this.getBlockImageName(this.tileMap[x][y]));
+                this.tileMapImages[x][y] = tileImage;
+            }
+        }
+    }
+
+    getBlockImageName(index) {
+        switch(index) {
+            case 1:
+                return 'block1';
+            case 2:
+                return 'block2';
+            case 3:
+                return 'block3';
+        }
     }
 
     addClickCash(event) {
@@ -85,6 +124,35 @@ export class MapScene extends Phaser.Scene {
         this.currentCashText.setText('$' + state.getCurrentCash());
     }
 
+    updateTileHighlight(x, y) {
+        let xDiff = x - this.mapOriginX;
+        let yDiff = -1 * (y - this.mapOriginY - (blockImageWidth / 2));
+        let tileX = Math.floor((xDiff / blockImageWidth / 2) + (yDiff / blockImageWidth));
+        let tileY = Math.floor((yDiff / blockImageWidth) - (xDiff / blockImageWidth / 2));
+
+        if (tileX == this.tileHighlightActiveX && tileY == this.tileHighlightActiveY) {
+            return;
+        }
+
+        // Update tints
+        if (this.areTileCoordinatesValid(this.tileHighlightActiveX, this.tileHighlightActiveY)) {
+            this.tileMapImages[this.tileHighlightActiveX][this.tileHighlightActiveY].setTint(0xffffff);
+        }
+
+        if (this.areTileCoordinatesValid(tileX, tileY)) {
+            this.tileMapImages[tileX][tileY].setTint(0xff7c73);
+            this.tileHighlightActiveX = tileX;
+            this.tileHighlightActiveY = tileY;
+        } else {
+            this.tileHighlightActiveX = -1;
+            this.tileHighlightActiveY = -1;
+        }
+    }
+
+    areTileCoordinatesValid(x, y) {
+        return x >= 0 && x < this.mapWidth && y >= 0 && y < this.mapHeight;
+    }
+
     // Add cash every second
     update() {
         let now = Date.now();
@@ -99,5 +167,7 @@ export class MapScene extends Phaser.Scene {
             let timeRemainder = timePassed - (secondsPassed * 1000);
             this.lastCashGrowth = now - timeRemainder;
         }
+        
+        this.updateTileHighlight(this.game.input.mousePointer.x, this.game.input.mousePointer.y);
     }
 }
